e6f12e0939543d712ac8dbd4798b7781bb39419a
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-01-22 23:23:27 +0530

    Added something I guess, gonna go to sleep now

---
+++ main.py
@@ -0,0 +1,10 @@
+red = "\x1b[91m"
+green = "\x1b[92m"
+yellow = "\x1b[93m"
+blue = "\x1b[94m"
+reset = "\x1b[0m"
+# Not sure why I'll need a traffic signal but hey!
+
+print(f"{blue}Hello there 👋")
+print(f"{blue}A {yellow}🔥 warm 🔥{blue} welcome to {green}🐢 Turtle Flags {reset}!")
+country_input = input(f"{yellow}Please enter the country's flag you want to see: {red}")
