7b20331b61d47c181a9b6e535cf1dd327c6067bc
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-01-30 20:44:23 +0530

    Improved Russia

--- flags.py
+++ flags.py
@@ -122,21 +122,21 @@ def russia(flag: Turtle, height: int):
 
 	# Setting up
 	flag.up()
-	flag.goto(width / 2, height / 6)
+	flag.goto(width / 2, height / 2)
 	flag.down()
 
-	# Drawing the white rectangle
-	flag.color(255, 255, 255)
+	# Drawing red rectangle
+	flag.color(228, 24, 28)
 	flag.begin_fill()
 
+	flag.right(180)
+	flag.forward(width)
 	flag.left(90)
-	flag.forward(height / 3)
+	flag.forward(height)
 	flag.left(90)
 	flag.forward(width)
 	flag.left(90)
-	flag.forward(height / 3)
-	flag.left(90)
-	flag.forward(width)
+	flag.forward(height)
 
 	flag.end_fill()
 
@@ -144,33 +144,29 @@ def russia(flag: Turtle, height: int):
 	flag.color(28, 53, 120)
 	flag.begin_fill()
 
-	flag.right(90)
-	flag.forward(height / 3)
-	flag.right(90)
+	flag.left(90)
 	flag.forward(width)
-	flag.right(90)
-	flag.forward(height / 3)
-	flag.right(90)
+	flag.left(90)
+	flag.forward(height * (2 / 3))
+	flag.left(90)
 	flag.forward(width)
+	flag.left(90)
+	flag.forward(height * (2 / 3))
 
 	flag.end_fill()
 
-	# Drawing the red rectangle
-	flag.up()
-	flag.goto(width / 2, -1 * (height / 6))
-	flag.down()
-
-	flag.color(228, 24, 28)
+	# Drawing the white rectangle
+	flag.color(255, 255, 255)
 	flag.begin_fill()
 
-	flag.right(90)
-	flag.forward(height / 3)
-	flag.right(90)
+	flag.left(90)
 	flag.forward(width)
-	flag.right(90)
+	flag.left(90)
 	flag.forward(height / 3)
-	flag.right(90)
+	flag.left(90)
 	flag.forward(width)
+	flag.left(90)
+	flag.forward(height / 3)
 
 	flag.end_fill()
 
@@ -182,21 +178,21 @@ def romania(flag: Turtle, height: int):
 
 	# Setting up
 	flag.up()
-	flag.goto(-1 * (width / 6), height / 2)
+	flag.goto(width / 2, height / 2)
 	flag.down()
 
 	# Drawing blue rectangle
 	flag.color(0, 43, 127)
 	flag.begin_fill()
 
-	flag.right(90)
+	flag.right(180)
+	flag.forward(width)
+	flag.left(90)
 	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
+	flag.left(90)
+	flag.forward(width)
+	flag.left(90)
 	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
 
 	flag.end_fill()
 
@@ -204,32 +200,29 @@ def romania(flag: Turtle, height: int):
 	flag.color(252, 209, 22)
 	flag.begin_fill()
 
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	flag.left(90)
+	flag.forward(width)
+	flag.left(90)
+	flag.forward(height * (2 / 3))
+	flag.left(90)
+	flag.forward(width)
+	flag.left(90)
+	flag.forward(height * (2 / 3))
 
 	flag.end_fill()
 
 	# Drawing the red rectangle
-	flag.up()
-	flag.goto(width / 6, height / 2)
-	flag.down()
-
 	flag.color(206, 17, 38)
 	flag.begin_fill()
 
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	flag.left(90)
+	flag.forward(width)
+	flag.left(90)
+	flag.forward(height / 3)
+	flag.left(90)
+	flag.forward(width)
+	flag.left(90)
+	flag.forward(height / 3)
 
 	flag.end_fill()
 
