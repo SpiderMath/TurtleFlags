8cce8e2feea793cb08c9606ce2f6e37310fb61dd
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-01-30 20:51:19 +0530

    Optimize Romania

--- flags.py
+++ flags.py
@@ -2,457 +2,460 @@ from turtle import Turtle
 
 
 # ------- Ukraine -------
-def ukraine(flag: Turtle, height: int):
+def ukraine(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
 
 	# Drawing the blue rectangle
-	flag.color(0, 87, 183)
-	flag.begin_fill()
+	pen.color(0, 87, 183)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the yellow rectangle
-	flag.color(255, 221, 0)
-	flag.begin_fill()
+	pen.color(255, 221, 0)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Indonesia -------
-def indonesia(flag: Turtle, height: int):
+def indonesia(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
 
 	# Drawing the red rectangle
-	flag.color(239, 51, 60)
-	flag.begin_fill()
+	pen.color(239, 51, 60)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the white rectangle
-	flag.color(255, 255, 255)
-	flag.begin_fill()
+	pen.color(255, 255, 255)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Poland -------
-def poland(flag: Turtle, height: int):
+def poland(pen: Turtle, height: int):
 	if not height: height = 500
 	width = int(1.6 * height)
 
 	# Drawing the white rectangle
-	flag.color(255, 255, 255)
-	flag.begin_fill()
+	pen.color(255, 255, 255)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the red rectangle
-	flag.color(210, 39, 48)
-	flag.begin_fill()
+	pen.color(210, 39, 48)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Russia -------
-def russia(flag: Turtle, height: int):
+def russia(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
 
 	# Setting up
-	flag.up()
-	flag.goto(width / 2, height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(width / 2, height / 2)
+	pen.down()
 
 	# Drawing red rectangle
-	flag.color(228, 24, 28)
-	flag.begin_fill()
+	pen.color(228, 24, 28)
+	pen.begin_fill()
 
-	flag.right(180)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height)
+	pen.right(180)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the blue rectangle
-	flag.color(28, 53, 120)
-	flag.begin_fill()
+	pen.color(28, 53, 120)
+	pen.begin_fill()
 
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height * (2 / 3))
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height * (2 / 3))
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height * (2 / 3))
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height * (2 / 3))
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the white rectangle
-	flag.color(255, 255, 255)
-	flag.begin_fill()
+	pen.color(255, 255, 255)
+	pen.begin_fill()
 
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 3)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 3)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height / 3)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height / 3)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Romania -------
-def romania(flag: Turtle, height: int):
+def romania(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
+	blue = (0, 43, 127)
+	red = (206, 17, 38)
+	yellow = (252, 209, 22)
 
 	# Setting up
-	flag.up()
-	flag.goto(width / 2, height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(width / 2, height / 2)
+	pen.down()
 
-	# Drawing blue rectangle
-	flag.color(0, 43, 127)
-	flag.begin_fill()
+	# Drawing the blue rectangle
+	pen.color(blue)
+	pen.begin_fill()
 
-	flag.right(180)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the yellow rectangle
-	flag.color(252, 209, 22)
-	flag.begin_fill()
+	pen.color(yellow)
+	pen.begin_fill()
 
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height * (2 / 3))
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height * (2 / 3))
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width * (2 / 3))
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width * (2 / 3))
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the red rectangle
-	flag.color(206, 17, 38)
-	flag.begin_fill()
+	pen.color(red)
+	pen.begin_fill()
 
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 3)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Chad -------
-def chad(flag: Turtle, height: int):
+def chad(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
 
 	# Setting up
-	flag.up()
-	flag.goto(-1 * (width / 6), height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(-1 * (width / 6), height / 2)
+	pen.down()
 
 	# Drawing blue rectangle
-	flag.color(0, 38, 100)
-	flag.begin_fill()
+	pen.color(0, 38, 100)
+	pen.begin_fill()
 
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the yellow rectangle
-	flag.color(254, 203, 0)
-	flag.begin_fill()
+	pen.color(254, 203, 0)
+	pen.begin_fill()
 
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the red rectangle
-	flag.up()
-	flag.goto(width / 6, height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(width / 6, height / 2)
+	pen.down()
 
-	flag.color(198, 12, 48)
-	flag.begin_fill()
+	pen.color(198, 12, 48)
+	pen.begin_fill()
 
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- France -------
-def france(flag: Turtle, height: int):
+def france(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
 
 	# Setting up
-	flag.up()
-	flag.goto(-1 * (width / 6), height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(-1 * (width / 6), height / 2)
+	pen.down()
 
 	# Drawing blue rectangle
-	flag.color(0, 35, 149)
-	flag.begin_fill()
+	pen.color(0, 35, 149)
+	pen.begin_fill()
 
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the white rectangle
-	flag.color(255, 255, 255)
-	flag.begin_fill()
+	pen.color(255, 255, 255)
+	pen.begin_fill()
 
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the red rectangle
-	flag.up()
-	flag.goto(width / 6, height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(width / 6, height / 2)
+	pen.down()
 
-	flag.color(237, 41, 57)
-	flag.begin_fill()
+	pen.color(237, 41, 57)
+	pen.begin_fill()
 
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Ireland -------
-def ireland(flag: Turtle, height: int):
+def ireland(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 2 * height
 
 	# Setting up
-	flag.up()
-	flag.goto(-1 * (width / 6), height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(-1 * (width / 6), height / 2)
+	pen.down()
 
 	# Drawing green rectangle
-	flag.color(0, 154, 68)
-	flag.begin_fill()
+	pen.color(0, 154, 68)
+	pen.begin_fill()
 
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the white rectangle
-	flag.color(255, 255, 255)
-	flag.begin_fill()
+	pen.color(255, 255, 255)
+	pen.begin_fill()
 
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the orange rectangle
-	flag.up()
-	flag.goto(width / 6, height / 2)
-	flag.down()
+	pen.up()
+	pen.goto(width / 6, height / 2)
+	pen.down()
 
-	flag.color(255, 130, 0)
-	flag.begin_fill()
+	pen.color(255, 130, 0)
+	pen.begin_fill()
 
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
-	flag.right(90)
-	flag.forward(width / 3)
-	flag.right(90)
-	flag.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
+	pen.right(90)
+	pen.forward(width / 3)
+	pen.right(90)
+	pen.forward(height)
 
-	flag.end_fill()
+	pen.end_fill()
 
 
 # ------- Colombia -------
-def colombia(flag: Turtle, height: int):
+def colombia(pen: Turtle, height: int):
 	if not height: height = 400
 	width = 1.5 * height
 
 	# Drawing the yellow rectangle
-	flag.color(255, 205, 0)
-	flag.begin_fill()
+	pen.color(255, 205, 0)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width)
-	flag.left(90)
-	flag.forward(height / 2)
-	flag.left(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width)
+	pen.left(90)
+	pen.forward(height / 2)
+	pen.left(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the red rectangle
-	flag.color(200, 16, 46)
-	flag.begin_fill()
+	pen.color(200, 16, 46)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width)
-	flag.right(90)
-	flag.forward(height / 2)
-	flag.right(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width)
+	pen.right(90)
+	pen.forward(height / 2)
+	pen.right(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
 
 	# Drawing the blue rectangle
-	flag.color(0, 48, 135)
-	flag.begin_fill()
+	pen.color(0, 48, 135)
+	pen.begin_fill()
 
-	flag.forward(width / 2)
-	flag.right(90)
-	flag.forward(height / 4)
-	flag.right(90)
-	flag.forward(width)
-	flag.right(90)
-	flag.forward(height / 4)
-	flag.right(90)
-	flag.forward(width / 2)
+	pen.forward(width / 2)
+	pen.right(90)
+	pen.forward(height / 4)
+	pen.right(90)
+	pen.forward(width)
+	pen.right(90)
+	pen.forward(height / 4)
+	pen.right(90)
+	pen.forward(width / 2)
 
-	flag.end_fill()
+	pen.end_fill()
